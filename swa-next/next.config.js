d7f454b781fd7dbc1521d37de84812f768e3249a
Neil Dobson <neil.dobson71@gmail.com>
2023-02-04 10:06:14 +1100

    Add Tailwind Salient template

--- swa-next/next.config.js
+++ swa-next/next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    scrollRestoration: true,
+  },
   images: {
     unoptimized: true,
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
